5e32bfc669b66a0fa069627771aecc3e4acb0a17
Oleksii Pudlo <pudloaleksey@gmail.com>
2024-12-31 15:25:58 +0100

    Enhance testing and configuration in package.json and vitest.config.ts
    - Added "type": "module" to package.json for ES module support.
    - Introduced new test scripts for database and unit testing in package.json.
    - Updated Prettier script to include a format check.
    - Added ESLint script for code linting.
    - Configured Vitest to include test files, set root directory, and specify Node environment in vitest.config.ts.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     // Your test configuration options
+    include: ['tests/**/*.test.ts'],
+    root: '.',
+    environment: 'node',
+    globals: true,
+    //setupFiles: ['./tests/db/utilities/testDBSetup.ts']
   },
 });
